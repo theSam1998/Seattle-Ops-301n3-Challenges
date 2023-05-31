8dfc01ad0f213a1c8aae60679f87b89fa1a8f13e
theSam1998 <2894sallan@gmail.com>
2023-05-31 04:21:09 +0000

    pushing prewritten OpsChal12 to github, to test functionality of git push

---
+++ OpsChal12.py
@@ -0,0 +1,39 @@
+#Author: Sam Allan
+#Date of last revision: 05/30/2023
+#Script: Code Fellows Ops 301n3 Ops Challenge 12
+#Purpose: Create a Python script that performs the following:
+#Prompt the user to type a string input as the variable for your destination URL.
+#Prompt the user to select a HTTP Method of the following options:
+#   GET
+#   POST
+#   PUT
+#   DELETE
+#   HEAD
+#   PATCH
+#   OPTIONS
+#Print to the screen the entire request your script is about to send. Ask the user to confirm before proceeding.
+#Using the requests library, perform a GET request against your lab web server.
+#For the given header, translate the codes into plain terms that print to the screen; for example, a 404 error should print Site not found to the terminal instead of 404.
+#For the given URL, print response header information to the screen.
+
+#Import space:
+
+
+#Global Variables:
+
+
+
+#MAIN
+
+
+
+
+
+
+
+
+
+
+
+
+#END
