f85ced36731f5206c11abb4264ed51fe7a43d983
theSam1998 <2894sallan@gmail.com>
2023-06-08 02:50:10 +0000

    pushing some improvements

--- OpsChal13.ps1
+++ OpsChal13.ps1
@@ -89,8 +89,14 @@ function NewGroup {
 function NewOU {
     $OUName = Read-Host -Prompt 'Enter the OU Name'
     $ouParams = @{
-        Name = $OUName
-        PassThru = $True
+    Name = $OUName
+    PassThru = $True
+    }
+    try {
+    New-ADOrganizationalUnit @ouParams
+    echo "OU created!"
+    } catch {
+    echo "Error creating OU: $_"
     }
 }
 
@@ -124,12 +130,12 @@ function ModifyGroup {
 
 # Function to modify an OU
 function ModifyOU {
-    $OUName = Read-Host -Prompt 'Enter the OU Name to modify'
+    $OUName = Read-Host -Prompt 'Enter the Distinguished Name of the OU to modify'
     $NewOUName = Read-Host -Prompt 'Enter the new OU Name'
     
     Set-ADOrganizationalUnit -Identity $OUName -Name $NewOUName
     echo "OU modified!"
-}
+   }
 
 function ModifyPassword {
     $SamAccountName = Read-Host -Prompt 'Enter the SamAccountName of the account you want to change the password for'
@@ -137,33 +143,66 @@ function ModifyPassword {
     Set-ADAccountPassword -Identity $SamAccountName -NewPassword $newPassword -Reset
     Write-Host "Password changed successfully!"
 }
+function AddUserToGroup {
+    $UserName = Read-Host -Prompt 'Enter the SamAccountName of the user'
+    $GroupName = Read-Host -Prompt 'Enter the name of the group'
+    try {
+    Add-ADGroupMember -Identity $GroupName -Members $UserName
+    echo "User added to group!"
+    } catch {
+    echo "Error adding user to group: $_"
+    }
+}
+function RemoveUser {
+    $UserName = Read-Host -Prompt 'Enter the SamAccountName of the user to remove'
+    try {
+    Remove-ADUser -Identity $UserName -Confirm:$false
+    echo "User removed!"
+    } catch {
+    echo "Error removing user: $_"
+    }
+}
+function ListUsers {
+    try {
+    Get-ADUser -Filter * | Format-Table SamAccountName, Name
+    } catch {
+    echo "Error listing users: $_"
+    }
+}
+function ShowHelp {
+    echo "1. Create a new user"
+    echo "2. Create a new group"
+    echo "3. Create a new OU"
+    echo "4. Modify a user"
+    echo "5. Modify a group"
+    echo "6. Modify an OU"
+    echo "7. Change a password"
+    echo "8. Add user to group"
+    echo "9. Remove user"
+    echo "10. List users"
+    echo "11. Exit"
+}
+      
+   
 # Menu system
 do {
-    # Display the menu
-    Write-Host "1. Create a new user"
-    Write-Host "2. Create a new group"
-    Write-Host "3. Create a new OU"
-    Write-Host "4. Modify a user"
-    Write-Host "5. Modify a group"
-    Write-Host "6. Modify an OU"
-    Write-Host "7. Change a password"
-    Write-Host "8. Exit"
-
+    ShowHelp
     $choice = Read-Host "Enter your choice"
-
-
     switch ($choice) {
-        '1' { NewUser }
-        '2' { NewGroup }
-        '3' { NewOU }
-        '4' { ModifyUser }
-        '5' { ModifyGroup }
-        '6' { ModifyOU }
-        '7' { ModifyPassword }
-        '8' { return }
-        default { Write-Host "Invalid choice, please try again." }
+    '1' { NewUser }
+    '2' { NewGroup }
+    '3' { NewOU }
+    '4' { ModifyUser }
+    '5' { ModifyGroup }
+    '6' { ModifyOU }
+    '7' { ModifyPassword }
+    '8' { AddUserToGroup }
+    '9' { RemoveUser }
+    '10' { ListUsers }
+    '11' { return }
+    default { Write-Host "Invalid choice, please try again." }
     }
-} while ($true)
-
+   } while ($true)
+   
 
 #END
